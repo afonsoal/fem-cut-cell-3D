5a26b3d792a80a34f8649cdbfeba720ff9dfd7a9
afonsoal <afonsoal@gmail.com>
2015-09-08 15:03:15 -0300

    License updates.

--- include/NewFace_3D.cpp
+++ include/NewFace_3D.cpp
@@ -1,8 +1,44 @@
-/*
+/* The fem-cut-cell-3D software
+ * Author	  : Afonso Alborghetti Londero (afonsoal@gmail.com)
+ * Last update:	08/Sep/2015
+ *
  * NewFace_3D.cpp
  *
- *  Created on: Nov 16, 2014
- *      Author: afonsoal
+ * This file is part of the fem-cut-cell-3D software, built using the deal.ii
+ * library. You are free to use it under the MIT License as described below.
+ *
+ *
+ * The MIT License (MIT)
+ * Copyright (c) <2015> <Afonso Alborghetti Londero>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.*/
+/* ---------------------------------------------------------------------
+ * deal.ii license information
+ *
+ * Copyright (C) 1999 - 2013 by the deal.II authors
+ *
+ * The deal.II library is free software; you can use it, redistribute
+ * it, and/or modify it under the terms of the GNU Lesser General
+ * Public License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * The full text of the license can be found in the file LICENSE at
+ * the top level of the deal.II distribution.
+ *
+ * ---------------------------------------------------------------------
+ *
  */
 
 #include "NewFace_3D.h"
